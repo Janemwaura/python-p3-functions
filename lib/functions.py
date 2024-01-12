933195387e08cbd68909d53f70abe505ad6e95c2
Janemwaura <mwaura461@gmail.com>
2024-01-12 21:15:04 +0300

    complete

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+    return name
+
+greet("Joy")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+    
+greet_with_default("Mark")    
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+result = add(4,9)
+print(result)
 
 def halve(number):
-    pass
+    return number / 2
+
+result = halve(40)
+print(result)
+
+
+
